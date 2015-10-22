fdc4f32c1c610e81b2f640b51048ddfda5c30d3e
tredyfa <tredyfa@fengxudeMacBook-Pro.local>
2015-10-22 09:39:08 +0800

    xx

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,57 @@
+#include "HelloWorldScene.h"
+#include "cocostudio/CocoStudio.h"
+
+using namespace cocostudio;
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+
+    ArmatureDataManager::getInstance()->addArmatureFileInfo("DemoPlayer0.png" , "DemoPlayer0.plist" , "DemoPlayer.ExportJson");
+    Armature *armature = Armature::create( "NewAnimation");
+    armature->setPosition(Point(visibleSize.width * 0.5, visibleSize.height * 0.5));
+    // 播放动画
+    armature->getAnimation()->play("walk");
+    this->addChild(armature);
+
+    // add "HelloWorld" splash screen"
+
+    
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
